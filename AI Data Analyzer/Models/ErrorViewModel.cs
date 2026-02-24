e24b5352b73e6b4323408880a1f229beda56d7a4
user <user@DESKTOP-N29PVVG>
2026-02-24 14:16:04 +0700

    Add project files.

---
+++ AI Data Analyzer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AI_Data_Analyzer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
